1096c38d95be7c1c8005eb19634da9093e2ff8fa
Cristian <cristian.antezana22@gmail.com>
2026-09-02 21:51:23 -0400

    Base con react y springboot tanstack

---
+++ cafeteria_unifranz/src/main/java/com/cafeteria/cafeteria_unifranz/CafeteriaUnifranzApplication.java
@@ -0,0 +1,13 @@
+package com.cafeteria.cafeteria_unifranz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaUnifranzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteriaUnifranzApplication.class, args);
+	}
+
+}
---
+++ cafeteria_unifranz/src/test/java/com/cafeteria/cafeteria_unifranz/CafeteriaUnifranzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeteria.cafeteria_unifranz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeteriaUnifranzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
